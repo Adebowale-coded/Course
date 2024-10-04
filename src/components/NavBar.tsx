0eff56d51362f3c7bbea8d23ec5c3345b1e630f7
Adebowale Adeoti <adebowale@alphamorgan.com>
2024-10-04 16:16:20 +0100

    final

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,19 +17,19 @@ const NavBar = ({bg}:props) => {
    console.log(NavClicked)
   return (
     <>
-        <nav className={`p-[30px] fixed flex items-center justify-between w-full z-10 ${bg}`}>
+        <nav className={` p-[30px] fixed flex lg:items-center flex-col lg:flex-row justify-between w-full z-10 ${bg}`}>
             <div className='flex items-center gap-[30px] '>
                <img src={Logo} alt="" className='' />
                <p className='text-white'>Browse</p> 
             </div>
             
-            <ul className='flex gap-[30px] items-center lg:hidden'>
+            <ul className={`transition-all duration-[1s] flex gap-[30px] items-center  h-0 ${NavClicked ? 'flex-col translate-y-[0px] h-[200px]' : 'flex-col translate-y-[-450px]'} md:flex `}>
                 <li className='text-white'>Become an Instructor</li>
                 <img src={shopping} alt="" className='' />
                 <img src={Icon} alt="" className='' />
                 <img src={girl} alt="" />
             </ul>
-            <img width="32" height="32" src="https://img.icons8.com/stamp/32/FFFFFF/menu.png" onClick={handleClick} alt="menu" className='lg:hidden md:hidden cursor-pointer'/>
+            <img width="32" height="32" src="https://img.icons8.com/stamp/32/FFFFFF/menu.png" onClick={handleClick} alt="menu" className='absolute right-[15px] lg:hidden md:hidden cursor-pointer'/>
         </nav>
     </>
   )
